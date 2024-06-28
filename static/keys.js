eb6227de588c61c684619c6c88bcb2841f7298f6
iCodeKaty <ca.katy@icodeschool.com>
2024-06-28 15:33:35 -0500

    Add files via upload

---
+++ static/keys.js
@@ -0,0 +1,7 @@
+export const hugging_face_key = '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'
+export const cloudinaryCloudname = "dmm8zr0az"
+export const sightengine_api_user = "491630136"
+export const sightengine_api_secret = "C4MoxqQPspV7jVvMv728pcy2sTpjDe9A"
+export const sightengine_workflow = "wfl_g5fdiHz4maLRX7ezhd867"
+export const sightengine_listID = "tli_g5t6wWqDfOy6QW00gUBbQ"
+export const elevenlabs_api_key = "sk_942b8253bf033318343e481633e228907feeadb765f340a3"
